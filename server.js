031c72a4a6ce204f14a45146262ac4558222e306
Lucas <viper20@poczta.fm>
2017-09-26 20:53:35 +0200

    fix

--- server.js
+++ server.js
@@ -1,25 +1,25 @@
-var fs = require('fs');
-var express = require('express');
-var bodyParser = require('body-parser');
-var app = express();
-var stringifyFile;
+const fs = require('fs');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+let stringifyFile;
 
 app.use(bodyParser.json());
 
 app.get('/getNote', (req, res) => {
     fs.readFile('./test.json', 'utf8', (err, data) => {
         if(err) throw(err);
-        fileContent = data;
+        stringifyFile = data;
         res.send(data);
     });   
 });
 
 app.post('/updateNote/:note', (req, res) => {
-    fs.writeFile('./test.json', stringifyFile, (req, res, next) => {
+    stringifyFile = req.params.note;
+    fs.appendFile('./test.json', stringifyFile, (err) => {
         if(err) throw(err);
         console.log('file updated');
     });
-    res.send(req.params.note);
 });
 
-app.listen(3002);
+app.listen(3010);
